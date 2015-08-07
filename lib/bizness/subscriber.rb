9f4298364e69a33b903054b5b796b96a80cc0871
Theo Mills <twmills@twmills.com>
2015-08-07 15:00:18 -0500

    Change method invocation.

--- lib/bizness/subscriber.rb
+++ lib/bizness/subscriber.rb
@@ -1,8 +1,8 @@
 module Bizness::Subscriber
-  def self.subscribe(*event_names)
+  def subscribe(*event_names)
     event_names.each do |event_name|
-      Hey.subscribe(event_name) do |event|
-        self.call!(event[:metadata])
+      Hey.subscribe!(event_name) do |event|
+        call!(event[:metadata])
       end
     end
   end
---
+++ spec/subscriber_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe Bizness::Subscriber do
+  let(:event_name) { "operations:cancel_account:succeeded" }
+
+  before do
+    MockOperation.extend(Bizness::Subscriber)
+    MockOperation.subscribe event_name
+  end
+
+  describe ".subscribe" do
+    it "executes the operation if the subscribed event is broadcasted" do
+      expect(MockOperation).to receive(:call!).with({ foo: "bar"})
+
+      Hey.subscribe!(event_name) do
+        Thread.current[:ugh] = true
+      end
+
+      Hey.publish!(event_name, { foo: "bar"} )
+    end
+  end
+end
