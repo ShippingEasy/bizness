3225924556c57ae27968f0bf03288a01ea0b2826
Theo Mills <twmills@twmills.com>
2015-08-12 15:32:58 -0500

    Remove context.

--- spec/support/mock_operation.rb
+++ spec/support/mock_operation.rb
@@ -1,5 +1,18 @@
 class MockOperation < Bizness::Operation
+  attr_reader :foo
+  attr_accessor :custom_message
+
+  def initialize(foo:)
+    @foo = foo
+  end
+
   def call
-    context.custom_message = "Operation completed"
+    self.custom_message = "Operation completed"
+  end
+
+  def to_h
+    hash = { foo: foo }
+    hash[:custom_message] = custom_message unless custom_message.nil?
+    hash
   end
 end
