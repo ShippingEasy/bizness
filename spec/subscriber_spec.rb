e2d3473fc79a53247275a4a0ad8e495fe3761097
Theo Mills <twmills@twmills.com>
2015-08-12 15:50:44 -0500

    The subscribe helper now takes an optional block to act a a builder.

--- spec/subscriber_spec.rb
+++ spec/subscriber_spec.rb
@@ -2,21 +2,34 @@ require "spec_helper"
 
 describe Bizness::Subscriber do
   let(:event_name) { "cancel_account:succeeded" }
-
-  before do
-    MockOperation.extend(Bizness::Subscriber)
-    MockOperation.subscribe event_name
-  end
+  let!(:widget) { Widget.create(name: "Foo") }
 
   describe ".subscribe" do
-    it "executes the operation if the subscribed event is broadcasted" do
-      expect(MockOperation).to receive(:call!).with({ foo: "bar"})
-
-      Hey.subscribe!(event_name) do
-        Thread.current[:ugh] = true
+    context "when configured with a block" do
+      before do
+        MockDataOperation.extend(Bizness::Subscriber)
+        MockDataOperation.subscribe(event_name) do |event_data|
+          found_widget = Widget.find(event_data[:widget_id])
+          MockDataOperation.new(widget: found_widget)
+        end
       end
 
-      Hey.publish!(event_name, { foo: "bar"} )
+      it "executes the operation if the subscribed event is broadcasted" do
+        expect_any_instance_of(MockDataOperation).to receive(:call!)
+        Hey.publish!(event_name, { widget_id: widget.id } )
+      end
+    end
+
+    context "when not configured with a block" do
+      before do
+        MockOperation.extend(Bizness::Subscriber)
+        MockOperation.subscribe(event_name)
+      end
+
+      it "executes the operation if the subscribed event is broadcasted" do
+        expect(MockOperation).to receive(:new).with(foo: "baz").and_call_original
+        Hey.publish!(event_name, { foo: "baz" } )
+      end
     end
   end
 end
---
+++ spec/support/mock_data_operation.rb
@@ -0,0 +1,15 @@
+class MockDataOperation < Bizness::Operation
+  attr_reader :widget
+
+  def initialize(widget:)
+    @widget = widget
+  end
+
+  def call
+    widget.update_column :name, SecureRandom.hex
+  end
+
+  def to_h
+    { widget_id: widget.id, name: widget.name }
+  end
+end
