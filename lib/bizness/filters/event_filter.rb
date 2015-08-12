dde64a29c9f58f3ff5bff42cfebef108304f854b
Theo Mills <twmills@twmills.com>
2015-08-12 15:13:57 -0500

    Remove context and handle anything that is passed in.
    The event filter attempts to build its payload by calling `.to_h` or
    use the result of the operation if it’s a hash. Otherwise, it uses an
    empty hash.

--- lib/bizness/filters/event_filter.rb
+++ lib/bizness/filters/event_filter.rb
@@ -1,7 +1,7 @@
 module Bizness::Filters
   class EventFilter < Bizness::Filters::BaseFilter
     def call
-      Hey.publish!("#{event_name}:executed", context.to_h) do
+      Hey.publish!("#{event_name}:executed", payload) do
         evented_call
       end
     end
@@ -9,16 +9,25 @@ module Bizness::Filters
     private
 
     def evented_call
-      filtered_operation.call
-      Hey.publish!("#{event_name}:#{successful? ? "succeeded" : "failed"}", context.to_h)
+      result = filtered_operation.call
+      Hey.publish!("#{event_name}:#{successful? ? "succeeded" : "failed"}", payload(result))
     rescue => e
-      context.error = e.message
-      Hey.publish!("#{event_name}:failed", context.to_h)
+      Hey.publish!("#{event_name}:failed", payload.merge(error: e.message))
       raise e
     end
 
     def event_name
       __original_operation__.class.name.underscore.gsub("/", ":")
+    end
+
+    def payload(result = nil)
+      if self.respond_to?(:to_h)
+        to_h
+      elsif result.is_a?(Hash)
+        result
+      else
+        Hash.new
+      end
     end
   end
 end
--- spec/filters/event_filter_spec.rb
+++ spec/filters/event_filter_spec.rb
@@ -21,11 +21,11 @@ describe Bizness::Filters::EventFilter do
   context "when failed" do
     it "publishes a failed event" do
       op = MockOperation.new(foo: "bar")
-      op.fail!(error: "oops")
+      allow(op).to receive(:call).and_raise("Oops")
       filter = Bizness::Filters::EventFilter.new(op)
       expect(Hey).to receive(:publish!).once.with("mock_operation:executed", {foo: "bar"}).and_call_original
-      expect(Hey).to receive(:publish!).once.with("mock_operation:failed", {foo: "bar", custom_message: "Operation completed"})
-      filter.call
+      expect(Hey).to receive(:publish!).once.with("mock_operation:failed", {foo: "bar", error: "Oops"})
+      expect { filter.call }.to raise_error("Oops")
     end
   end
 end
